1e0b16209da0c43d6b3eed275005360615121f6e
John Ivan Puayap <johnivanpuayap@gmail.com>
2022-12-17 22:36:42 +0800

    Complete FoodOrder

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -19,7 +19,49 @@ public class FoodOrderGUI extends JFrame{
     private JCheckBox cSundae;
 
     public FoodOrderGUI() {
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                boolean foodSelected = false;
+                boolean discountSelected = false;
+                double total = 0;
 
+                if(cPizza.isSelected()){
+                    total += 100;
+                    foodSelected = true;
+                }
+                if(cBurger.isSelected()){
+                    total += 80;
+                    foodSelected = true;
+                }
+                if(cFries.isSelected()){
+                    total += 65;
+                    foodSelected = true;
+                }
+                if(cSoftDrinks.isSelected()){
+                    total += 55;
+                    foodSelected = true;
+                }
+                if(cTea.isSelected()){
+                    total += 50;
+                    foodSelected = true;
+                }
+                if(cSundae.isSelected()){
+                    total += 40;
+                    foodSelected = true;
+                }
+
+                if(rb5.isSelected()){
+                    total = total - (total * 0.05);
+                }else if(rb10.isSelected()){
+                    total = total - (total * 0.10);
+                }else if(rb15.isSelected()){
+                    total = total - (total * 0.15);
+                }
+                JOptionPane.showMessageDialog(panel1, "The total is Php" + String.format("%.2f", total));
+            }
+
+        });
     }
     public static void main(String[] args) {
         FoodOrderGUI order = new FoodOrderGUI();
