bfc6b93b7ad42990a5bc806f690f0deb56ac307b
John Ivan Puayap <johnivanpuayap@gmail.com>
2022-12-17 21:40:26 +0800

    Complete LeapYear

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -17,39 +17,23 @@ public class LeapYearGUI extends JFrame{
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                try {
-                    double year = Double.parseDouble(tfYear.getText());
-                    boolean isLeapYear = false;
+                double year = Double.parseDouble(tfYear.getText());
+                boolean isLeapYear = false;
 
-                    if(year < 0) {
-                        throw (new NegativeInputException());
-                    }
-
-                    if(year == 0) {
-                        throw (new ZeroInputException());
-                    }
-
-                    if (year % 4 == 0) {
-                        if (year % 100 == 0) {
-                            if (year % 400 == 0) {
-                                isLeapYear = true;
-                            }
-                        } else {
+                if (year % 4 == 0) {
+                    if (year % 100 == 0) {
+                        if (year % 400 == 0) {
                             isLeapYear = true;
                         }
-                    }
-
-                    if (isLeapYear) {
-                        JOptionPane.showMessageDialog(panel1, "Leap Year");
                     } else {
-                        JOptionPane.showMessageDialog(panel1, "Not a Leap Year");
+                        isLeapYear = true;
                     }
-                } catch(NumberFormatException numberFormatException) {
-                    JOptionPane.showMessageDialog(panel1, "Input should be a number", "Error", JOptionPane.ERROR_MESSAGE);
-                } catch (NegativeInputException negativeNumberException) {
-                    JOptionPane.showMessageDialog(panel1, "Input should not be a negative number", "Error", JOptionPane.ERROR_MESSAGE);
-                } catch (ZeroInputException zeroInputException) {
-                    JOptionPane.showMessageDialog(panel1, "Input should not be zero", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+
+                if (isLeapYear) {
+                    JOptionPane.showMessageDialog(panel1, "Leap Year");
+                } else {
+                    JOptionPane.showMessageDialog(panel1, "Not a Leap Year");
                 }
             }
         });
@@ -62,17 +46,6 @@ public class LeapYearGUI extends JFrame{
         leap.setDefaultCloseOperation(EXIT_ON_CLOSE);
         leap.setVisible(true);
         leap.setTitle("Leap Year Checker");
-    }
-
-    public static class ZeroInputException extends Exception {
-        public ZeroInputException () {
-            super("Input must be nonzero");
-        }
-    }
-    public static class NegativeInputException extends Exception {
-        NegativeInputException() {
-            super("Input must not be a negative number");
-        }
     }
 
 }
