044a6418ae44c837b5ec8bb3c074cf943a399576
John Ivan Puayap <johnivanpuayap@gmail.com>
2022-12-17 21:47:45 +0800

    Rename SimpleCalc components

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,10 +1,14 @@
 package SimpleCalc;
 
+import LeapYear.LeapYearGUI;
+
 import javax.swing.*;
 
-public class SimpleCalcGUI {
+public class SimpleCalcGUI extends JFrame {
     private JPanel panel1;
-    private JTextField textField1;
-    private JComboBox comboBox1;
-    private JButton computeResultButton;
+    private JTextField tfNumber1;
+    private JComboBox cbOperations;
+    private JButton btnCompute;
+    private JTextField tfNumber2;
+    private JTextField lblResult;
 }
