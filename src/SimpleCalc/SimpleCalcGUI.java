fcb79ed69a0227232646f1e1660e000068496998
John Ivan Puayap <johnivanpuayap@gmail.com>
2022-12-17 21:39:16 +0800

    Add LeapYear main function

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -4,9 +4,7 @@ import javax.swing.*;
 
 public class SimpleCalcGUI {
     private JPanel panel1;
-    private JTextField tfNumber1;
-    private JComboBox cbOperations;
-    private JButton btnCompute;
-    private JTextField tfNumber2;
-    private JLabel lblResult;
+    private JTextField textField1;
+    private JComboBox comboBox1;
+    private JButton computeResultButton;
 }
