28bdabd3e115df08798eb5e2e0659dfd4fb8d1f5
John Ivan Puayap <johnivanpuayap@gmail.com>
2022-12-17 22:43:58 +0800

    Add exception handling to FoodOrder

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -7,57 +7,67 @@ import java.awt.event.ActionListener;
 public class FoodOrderGUI extends JFrame{
     private JPanel panel1;
     private JCheckBox cPizza;
-    private JRadioButton rbNone;
-    private JButton btnOrder;
     private JCheckBox cBurger;
     private JCheckBox cFries;
     private JCheckBox cSoftDrinks;
     private JCheckBox cTea;
+    private JCheckBox cSundae;
+    private JRadioButton rbNone;
     private JRadioButton rb5;
     private JRadioButton rb10;
     private JRadioButton rb15;
-    private JCheckBox cSundae;
+    private JButton btnOrder;
 
     public FoodOrderGUI() {
         btnOrder.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                boolean foodSelected = false;
-                double total = 0;
+                try {
+                    boolean foodSelected = false;
+                    double total = 0;
 
-                if(cPizza.isSelected()){
-                    total += 100;
-                    foodSelected = true;
-                }
-                if(cBurger.isSelected()){
-                    total += 80;
-                    foodSelected = true;
-                }
-                if(cFries.isSelected()){
-                    total += 65;
-                    foodSelected = true;
-                }
-                if(cSoftDrinks.isSelected()){
-                    total += 55;
-                    foodSelected = true;
-                }
-                if(cTea.isSelected()){
-                    total += 50;
-                    foodSelected = true;
-                }
-                if(cSundae.isSelected()){
-                    total += 40;
-                    foodSelected = true;
-                }
+                    if(cPizza.isSelected()){
+                        total += 100;
+                        foodSelected = true;
+                    }
+                    if(cBurger.isSelected()){
+                        total += 80;
+                        foodSelected = true;
+                    }
+                    if(cFries.isSelected()){
+                        total += 65;
+                        foodSelected = true;
+                    }
+                    if(cSoftDrinks.isSelected()){
+                        total += 55;
+                        foodSelected = true;
+                    }
+                    if(cTea.isSelected()){
+                        total += 50;
+                        foodSelected = true;
+                    }
+                    if(cSundae.isSelected()){
+                        total += 40;
+                        foodSelected = true;
+                    }
 
-                if(rb5.isSelected()){
-                    total = total - (total * 0.05);
-                }else if(rb10.isSelected()){
-                    total = total - (total * 0.10);
-                }else if(rb15.isSelected()){
-                    total = total - (total * 0.15);
+                    if(rb5.isSelected()){
+                        total = total - (total * 0.05);
+                    }else if(rb10.isSelected()){
+                        total = total - (total * 0.10);
+                    }else if(rb15.isSelected()){
+                        total = total - (total * 0.15);
+                    }
+
+                    if(!foodSelected) {
+                        throw (new NoFoodSelectedException());
+                    }
+
+                    JOptionPane.showMessageDialog(panel1, "The total is Php" + String.format("%.2f", total));
+
+                } catch (NoFoodSelectedException noFoodSelectedException) {
+                    JOptionPane.showMessageDialog(panel1, "Please Select Food", "No Food Selected", JOptionPane.ERROR_MESSAGE);
                 }
-                JOptionPane.showMessageDialog(panel1, "The total is Php" + String.format("%.2f", total));
             }
 
         });
@@ -69,5 +79,11 @@ public class FoodOrderGUI extends JFrame{
         order.setDefaultCloseOperation(EXIT_ON_CLOSE);
         order.setVisible(true);
         order.setTitle("Simple Calculator");
+    }
+
+    public static class NoFoodSelectedException extends Exception {
+        public NoFoodSelectedException () {
+            super("Please select Food");
+        }
     }
 }
