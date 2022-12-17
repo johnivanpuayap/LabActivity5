09f67327d7324d9bae8a632b3f3ebc6c911020f9
John Ivan Puayap <johnivanpuayap@gmail.com>
2022-12-17 21:10:10 +0800

    Added more Exceptions

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -20,6 +20,15 @@ public class LeapYearGUI extends JFrame{
                 try {
                     double year = Double.parseDouble(tfYear.getText());
                     boolean isLeapYear = false;
+
+                    if(year < 0) {
+                        throw (new NegativeInputException());
+                    }
+
+                    if(year == 0) {
+                        throw (new ZeroInputException());
+                    }
+
                     if (year % 4 == 0) {
                         if (year % 100 == 0) {
                             if (year % 400 == 0) {
@@ -36,7 +45,11 @@ public class LeapYearGUI extends JFrame{
                         JOptionPane.showMessageDialog(panel1, "Not a Leap Year");
                     }
                 } catch(NumberFormatException numberFormatException) {
-                    JOptionPane.showMessageDialog(panel1, "Input should be a number", "numberFormatException", JOptionPane.ERROR_MESSAGE);
+                    JOptionPane.showMessageDialog(panel1, "Input should be a number", "Error", JOptionPane.ERROR_MESSAGE);
+                } catch (NegativeInputException negativeNumberException) {
+                    JOptionPane.showMessageDialog(panel1, "Input should not be a negative number", "Error", JOptionPane.ERROR_MESSAGE);
+                } catch (ZeroInputException zeroInputException) {
+                    JOptionPane.showMessageDialog(panel1, "Input should not be zero", "Error", JOptionPane.ERROR_MESSAGE);
                 }
             }
         });
@@ -50,4 +63,16 @@ public class LeapYearGUI extends JFrame{
         leap.setVisible(true);
         leap.setTitle("Leap Year Checker");
     }
+
+    public static class ZeroInputException extends Exception {
+        public ZeroInputException () {
+            super("Input must be nonzero");
+        }
+    }
+    public static class NegativeInputException extends Exception {
+        NegativeInputException() {
+            super("Input must not be a negative number");
+        }
+    }
+
 }
