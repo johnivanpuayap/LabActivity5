595cb77cb7d5e3008a9808ba18e94593b01b4a3f
John Ivan Puayap <johnivanpuayap@gmail.com>
2022-12-17 21:48:33 +0800

    Add SimpleCalc main function

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -11,4 +11,16 @@ public class SimpleCalcGUI extends JFrame {
     private JButton btnCompute;
     private JTextField tfNumber2;
     private JTextField lblResult;
+
+    public SimpleCalcGUI() {
+
+    }
+    public static void main(String[] args){
+        SimpleCalcGUI calculator = new SimpleCalcGUI();
+        calculator.setContentPane(calculator .panel1);
+        calculator.setSize(500, 500);
+        calculator.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        calculator.setVisible(true);
+        calculator.setTitle("Leap Year Checker");
+    }
 }
