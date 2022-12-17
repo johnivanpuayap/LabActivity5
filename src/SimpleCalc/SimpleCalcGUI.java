d6198e33b38aa0bf6068175da87d5c9b53738ba6
John Ivan Puayap <johnivanpuayap@gmail.com>
2022-12-17 22:06:48 +0800

    Add exception handling to SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -16,24 +16,30 @@ public class SimpleCalcGUI extends JFrame {
         btnCompute.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String operation = (String) cbOperations.getSelectedItem();
-                int n1 = Integer.parseInt(tfNumber1.getText());
-                int n2 = Integer.parseInt(tfNumber2.getText());
-                int result = 0;
+                try {
+                    String operation = (String) cbOperations.getSelectedItem();
+                    int n1 = Integer.parseInt(tfNumber1.getText());
+                    int n2 = Integer.parseInt(tfNumber2.getText());
+                    int result = 0;
 
-                if (operation.equals("+")) {
-                    result = n1 + n2;
+                    if (operation.equals("+")) {
+                        result = n1 + n2;
+                    }
+                    if (operation.equals("-")) {
+                        result = n1 - n2;
+                    }
+                    if (operation.equals("*")) {
+                        result = n1 * n2;
+                    }
+                    if (operation.equals("/")) {
+                        result = n1 / n2;
+                    }
+                    lblResult.setText(String.valueOf(result));
+                } catch (ArithmeticException arithmeticException) {
+                    JOptionPane.showMessageDialog(panel1, "Division by Zero is not possible", "Error", JOptionPane.ERROR_MESSAGE);
+                } catch (NumberFormatException numberFormatException) {
+                    JOptionPane.showMessageDialog(panel1, "Input should be a number", "Error", JOptionPane.ERROR_MESSAGE);
                 }
-                if (operation.equals("-")) {
-                    result = n1 - n2;
-                }
-                if (operation.equals("*")) {
-                    result = n1 * n2;
-                }
-                if (operation.equals("/")) {
-                    result = n1 / n2;
-                }
-                lblResult.setText(String.valueOf(result));
             }
         });
     }
